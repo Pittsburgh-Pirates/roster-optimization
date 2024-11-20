80c8c7a3b60ae0069d722acd2941321266b843d1
ryan-schreiber1 <ryan.schreiber@pirates.com>
2024-11-20 10:48:57 -0500

    adding zip files

---
+++ Pitching/00_Load_Pitching_Data.R
@@ -0,0 +1,21 @@
+library(dplyr)
+library(lpSolve)
+library(tidyr)
+library(purrr)
+library(ggplot2)
+
+setwd("~/Documents/Research/Analytics%20Dev/Working/KTENENBAUM/Roster_Optimization") 
+
+# Do we need to do this on a per inning basis rather than BF? Or RA9 rather than ERA?
+projRaw <- read.csv("EPS 2025 Pitchers.csv") %>% filter(ERA > 0 & SplitTypeCode == 'EPS' & 
+                                                          PrefPos %in% c('RHS','RHR','LHS','LHR','RHP','LHP','P') &
+                                                          !(OrgCode %in% c('NULL','NPB','KBO','BOC')))
+
+projRaw <- projRaw %>% mutate(
+  SP = as.numeric(PrefPos %in% c('LHS','RHS')),
+  RA_adj = ERA # come back to this value and make it better.
+) %>% select(PiratesID, PlayerName, OrgCode, SP, RA9_raw = ERA, RA_adj)
+
+
+
+
---
+++ Position_Players/00_Process_Data.R
@@ -0,0 +1,27 @@
+library(dplyr)
+library(lpSolve)
+library(tidyr)
+library(purrr)
+library(ggplot2)
+
+setwd("~/Documents/Research/Analytics%20Dev/Working/KTENENBAUM/Roster_Optimization")
+
+
+
+
+projRaw <- read.csv("~/Downloads/EPS.csv")
+offenseOnly <- projRaw %>% group_by(PiratesID, PlayerName, OrgCode, SplitType) %>% summarise(RAA_635 = mean(RAA_635))
+
+posFrame <- expand_grid(PiratesID = unique(projRaw$PiratesID), SplitType = unique(projRaw$SplitType), PositionTypeID = 2:10)
+
+defProjExp <- posFrame  %>% left_join(projRaw, by = c('PiratesID', 'PositionTypeID', 'SplitType')) %>% #filter(PiratesID =='86D4DD35-6065-4C58-B179-142B3EB1F261') %>% arrange(PositionTypeID) %>% 
+  select(PiratesID, PlayerName, SplitType, PositionTypeID, FRAA_150) %>% 
+  mutate(FRAA = ifelse(is.na(FRAA_150), -1000, FRAA_150)) %>% select(PiratesID, SplitType, PositionTypeID, FRAA)
+
+
+fullProj <- defProjExp %>% inner_join(offenseOnly, by = c('PiratesID', 'SplitType')) %>% 
+  mutate(Value = RAA_635 + FRAA)
+
+
+positions <- c('C','1B','2B','3B','SS','LF','CF','RF','DH')
+
