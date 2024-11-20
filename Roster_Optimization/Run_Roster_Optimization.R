654279fa94f99e7953b7f525e22572b6703d45fe
jshomar-pirates <joseph.shomar@pirates.com>
2024-11-20 15:56:59 -0500

    Add files via upload

--- Roster_Optimization/Run_Roster_Optimization.R
+++ Roster_Optimization/Run_Roster_Optimization.R
@@ -11,17 +11,22 @@ source("Pitching/02_Simulate_Pitching.R")
 ### Add Player Parameters ###
 # Position Players
 addPositionPlayers <- c(
-  "920aebff-8966-4696-aa37-275a785fd06b" # Ward
+  # ""
+  "1c40ff76-048b-4ff2-914a-70a0856f0f06", # Bryson Stott
+  "72f94915-e477-4e2e-b2c8-b7febe2484a2" # Brandon Marsh
 )
 playerRemovals_pos <- c(
-  "4ceb4b6e-f394-4027-b84e-6f02fb0b758a"
+  #""
+  "915c8fee-00aa-4f5f-9526-3f5d233f5603", # Jared Jones
+  "86d4dd35-6065-4c58-b179-142b3eb1f261" # IKF
 )
 addPlayerPositions <- data.frame(
   PiratesID = c("2dac7862-21dd-4926-8452-075757d9cccb", # Reynolds
-                "50ec0fe4-8960-4188-a1a3-36aae400e1e3"# Yorke
+                "50ec0fe4-8960-4188-a1a3-36aae400e1e3",# Yorke
+                "48b0cda6-abea-40ec-8120-c7ee12ceb3e6"#Cruz
   ),
-  PositionTypeID = c(3, 8),
-  FRAA = c(-2, -7)
+  PositionTypeID = c(3, 8, 6),
+  FRAA = c(-2, -7, -99)
 )
 playerConstraints <- data.frame(
   PiratesID = "48b0cda6-abea-40ec-8120-c7ee12ceb3e6", # Cruz
@@ -35,12 +40,12 @@ playerConstraints <- data.frame(
 #   adj = c(7, -3)
 # )
 addPitchers <- c(
-  "fcbbe69a-e6ea-464d-ade7-76606878be83"
+  ""
 )
 
 # Optimize Position Players
 base_position_players <- optimize_rosters_agg_all(df_repeated, Org = 'PIT'
-                                                  , playerRemovals = playerRemovals_pos
+                                                  #, playerRemovals = playerRemovals_pos
                                                   , addPlayerPositions = addPlayerPositions
                                                   , playerConstraints = NULL
                                                   # , playerConstraints = playerConstraints
@@ -49,7 +54,7 @@ base_position_players <- optimize_rosters_agg_all(df_repeated, Org = 'PIT'
 
 updated_position_players <- optimize_rosters_agg_all(df_repeated, Org = 'PIT'
                                                      , playerAdditions = addPositionPlayers
-                                                     # , playerRemovals = playerRemovals_pos
+                                                     , playerRemovals = playerRemovals_pos
                                                      , addPlayerPositions = addPlayerPositions
                                                      , playerConstraints = NULL
                                                      # , playerConstraints = playerConstraints
@@ -99,9 +104,9 @@ win_projections <- PP_Wins %>% inner_join(Orgs_Pitching, by = 'Org') %>%
     Org, Wins, WAA.y, Win_Total
   )
 base_PIT_win <- win_projections %>% filter(Org == 'PIT') %>% pull(Win_Total)
-# winDist_base <- findWinDist(Wins = win_projections %>% filter(Org == 'PIT') %>% pull(Win_Total), 
-#                         playoff_cond_probs = playoff_cond_probs,
-#                         playoffFit = playoffFit) %>% mutate(Wins = round(Wins,1))
+winDist_base <- findWinDist(Wins = win_projections %>% filter(Org == 'PIT') %>% pull(Win_Total), 
+                        playoff_cond_probs = playoff_cond_probs,
+                        playoffFit = playoffFit) %>% mutate(Wins = round(Wins,1))
 
 winDist_udpated <- findWinDist(Wins = win_projections %>% filter(Org == 'PIT') %>% pull(Win_Total) + marginalGain_runs/10, 
                                playoff_cond_probs = playoff_cond_probs,
